37c4e2de4a6ac9c2f42c1ccb569973119cf1534a
GHouck <gchedu@outlook.com>
2015-09-02 14:04:27 -0400

    Added Note Controller and View.

---
+++ ElevenNote.Web/Controllers/NotesController.cs
@@ -0,0 +1,47 @@
+﻿using ElevenNote.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class NotesController : Controller
+    {
+        // GET: Notes
+        public ActionResult Index()
+        {
+            var Notes = new List<NoteListViewModel>();
+            Notes.Add(new NoteListViewModel()
+            {
+                Id = 1,
+                Title = "First!",
+                DateCreated = DateTime.UtcNow.AddMonths(-1),
+                DateModified = DateTime.UtcNow,
+                IsFavorite = false
+            });
+
+            var weirdNote = new NoteListViewModel();
+            weirdNote.Id = 2;
+            weirdNote.Title = "BUmmer";
+            weirdNote.DateCreated = DateTime.UtcNow;
+            weirdNote.DateModified = null;
+            weirdNote.IsFavorite = true;
+
+            Notes.Add(weirdNote);
+
+            Notes.Add(new NoteListViewModel()
+            {
+                Id = 4,
+                Title = "Fourd",
+                DateCreated = DateTime.Now,
+                DateModified = null,
+                IsFavorite = true
+
+            });
+
+            return View(Notes.OrderByDescending(Note => Note.DateCreated));
+        }
+    }
+}
